95d1e276672b948ab9e2cc97f7609708c93ed712
willwalker7 <billywalker2128@gmail.com>
2026-06-07 12:34:40 +0200

    update1: demo cat-regression

--- tests/test_cat_regression.py
+++ tests/test_cat_regression.py
@@ -1,94 +1,64 @@
 import pandas as pd
 import pytest
-from cat_regression import get_features_cat_regression, plot_features_cat_regression
+from toolbox_ml.eda.cat_regression import get_features_cat_regression, plot_features_cat_regression
 import math
 
 def test_get_features_cat_regression_ok():
-
     df = pd.DataFrame({
         "color": ["rojo"] * 4 + ["azul"] * 4,
-        "ventas": [100, 101, 102, 103, 1, 2, 3, 4]
-    })
-
+        "ventas": [100, 101, 102, 103, 1, 2, 3, 4]})
     resultado = get_features_cat_regression(
         df=df,
         target_col="ventas",
-        pvalue=0.05
-    )
-
+        pvalue=0.05)
     assert isinstance(resultado, list)
     assert "color" in resultado
     
 
 def test_get_features_cat_regression_sin_significativas():
-
     df = pd.DataFrame({
         "color": ["rojo", "azul", "verde", "amarillo"],
-        "ventas": [10, 11, 10, 11]
-    })
-
+        "ventas": [10, 11, 10, 11]})
     resultado = get_features_cat_regression(
         df=df,
-        target_col="ventas"
-    )
-
+        target_col="ventas")
     assert resultado == []
     
 
 def test_get_features_cat_regression_target_inexistente():
-
     df = pd.DataFrame({
-        "color": ["rojo", "azul"]
-    })
-
+        "color": ["rojo", "azul"]})
     resultado = get_features_cat_regression(
         df=df,
-        target_col="ventas"
-    )
-
+        target_col="ventas")
     assert resultado is None
     
 
 def test_plot_features_cat_regression_ok():
-
     df = pd.DataFrame({
         "color": ["rojo"] * 4 + ["azul"] * 4,
-        "ventas": [100, 101, 102, 103, 1, 2, 3, 4]
-    })
-
+        "ventas": [100, 101, 102, 103, 1, 2, 3, 4]})
     resultado = plot_features_cat_regression(
         df=df,
-        target_col="ventas"
-    )
-
+        target_col="ventas")
     assert isinstance(resultado, list)
     assert "color" in resultado
     
 
 def test_plot_features_cat_regression_sin_significativas():
-
     df = pd.DataFrame({
         "color": ["rojo", "azul", "verde", "amarillo"],
-        "ventas": [10, 11, 10, 11]
-    })
-
+        "ventas": [10, 11, 10, 11]})
     resultado = plot_features_cat_regression(
         df=df,
-        target_col="ventas"
-    )
-
+        target_col="ventas")
     assert resultado == []
     
 
 def test_plot_features_cat_regression_target_inexistente():
-
     df = pd.DataFrame({
-        "color": ["rojo", "azul"]
-    })
-
+        "color": ["rojo", "azul"]})
     resultado = plot_features_cat_regression(
         df=df,
-        target_col="ventas"
-    )
-
+        target_col="ventas")
     assert resultado is None
